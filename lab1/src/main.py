25d7bc7bd2542eebf813e3f5dc9baffc89f04541
Lukasz1928 <Lukasz19281@gmail.com>
2019-03-16 22:38:49 +0100

    Finished third task

--- lab1/src/main.py
+++ lab1/src/main.py
@@ -2,9 +2,8 @@ import regex
 
 from src.external import find_external_references
 from src.file_utils import get_data, pprint
+from src.occurences import count_ustawa_occurrences
 
 if __name__ == "__main__":
     data = get_data()
-    x = find_external_references(data)
-    for _x in x[0]:
-        print(_x)
+    count_ustawa_occurrences(data, "ustawa_word_count")
--- lab1/src/occurences.py
+++ lab1/src/occurences.py
@@ -1,4 +1,28 @@
+import regex
 
 
-def count_ustawa_occurrences():
-    return 0
+def count_ustawa_occurrences(data, filename="occurences"):
+    text = merge_data(data)
+    search_regex = r'(\b[Uu][Ss][Tt][Aa][Ww]([Aa]|[Aa][Cc][Hh]|[Aa][Mm][Ii]|[Ąą]|[Ęę]|[Ii][Ee]|[Oo][Mm]|[Oo]|[Yy])?\b)'
+    occurrences = regex.findall(search_regex, text)
+    lower_occurrences = [o[0].lower() for o in occurrences]
+    unique_occurrences = set(lower_occurrences)
+    unique_occurrences_count = {}
+    for uo in unique_occurrences:
+        unique_occurrences_count[uo] = lower_occurrences.count(uo)
+    save_result(len(occurrences), unique_occurrences_count, filename)
+
+
+def merge_data(data):
+    s = ""
+    for d in data:
+        s += d
+    return s
+
+
+def save_result(all_count, inflected_count, filename):
+    with open("results/{}.txt".format(filename), "w") as f:
+        f.write("Total count of all forms of 'ustawa' word: {}\n".format(all_count))
+        f.write("Total count of inflected forms of 'ustawa' word:\n")
+        for uo in inflected_count:
+            f.write("\t{}: {}\n".format(uo, inflected_count[uo]))
