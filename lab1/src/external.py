54fc4c2083d8a84312b952236ae9bbaa1354973f
Lukasz1928 <Lukasz19281@gmail.com>
2019-03-19 10:53:00 +0100

    Finished external references

--- lab1/src/external.py
+++ lab1/src/external.py
@@ -8,6 +8,7 @@ def find_external_references(data, filename):
             references.extend(find_external_in_single_file(data[k]))
         except TypeError:
             pass # file in incorrect format
+
     aggregated = aggregate_results(references)
     dec_keys = sorted(aggregated.keys(), key=lambda x : aggregated[x]['count'], reverse=True)
     with open('results/{}.txt'.format(filename), "w") as file:
@@ -34,25 +35,37 @@ def find_external_in_single_file(text):
     title_regex = r'(?P<title>(\s*(\bo\b)|-)\s*(\w|\s)*?)(?=\s*\()'
     journal_regex = r'\(\s*[Dd]z\.\s*U.\s+(?P<journal_details>(.|\s)*?)\)'
 
-    journal_year_groups_regex = r'((z\s+(?P<journal_year>\d{4})\s+r.\s+)?(?P<numbers>(([Nn]r\s+\d+,\s+poz\.\s+\d+\s*(,|i|oraz|$)\s*))+))'
-    journal_numbers_regex = r'[Nn]r\s+(?P<number>\d+),\s+poz\.\s+(?P<position>\d+)'
+    journal_year_groups_regex = r'((z\s+(?P<journal_year>\d{4})\s+r.\s+)?(?P<numbers>(([Nn]r\s+\d+,(\s*(poz\.)?\s*\d+\s*(,|i|oraz|$)\s*)+))+))'
+    journal_numbers_regex = r'[Nn]r\s+(?P<number>\d+),\s*(?P<positions>((poz\.)?\s*\d+\s*(i|,|oraz|$)?)+)'
+    journal_position_regex = r'(poz.)?\s*(?P<position>\d+)'
 
-    reference_regex = r'\b[Uu]staw.+\s+({})\s*({})\s*({})'.format(date_regex, title_regex, journal_regex)
+    reference_regex = r'(\b[Uu]staw.+\s+({})\s*({})\s*({}))|(w\s+Dz\.\s*U\.\s+((?P<journals>(.|\s)*?\d)(?=\.)))'.format(date_regex, title_regex, journal_regex)
 
     references = []
     for ref in regex.finditer(reference_regex, text, regex.MULTILINE):
         ref_data = {}
-        ref_data['day'] = ref.group('day')
-        ref_data['month'] = ref.group('month')
-        ref_data['year'] = ref.group('year')
-        ref_data['title'] = format_title(ref.group('title'))
-        ref_data['journals'] = []
-        for year_group in regex.finditer(journal_year_groups_regex, ref.group('journal_details'), regex.MULTILINE):
-            year = year_group.group('journal_year')
-            for journal_data in regex.finditer(journal_numbers_regex, year_group.group('numbers'), regex.MULTILINE):
-                ref_data['journals'].append({'number': journal_data.group('number'),
-                                             'position': journal_data.group('position'),
-                                             'year': year if year is not None else ref_data['year']})
+        if ref.group('journals') is not None:
+            ref_data['journals'] = []
+            for year_group in regex.finditer(journal_year_groups_regex, ref.group('journals'), regex.MULTILINE):
+                year = year_group.group('journal_year')
+                for number_group in regex.finditer(journal_numbers_regex, year_group.group('numbers'), regex.MULTILINE):
+                    number = number_group.group('number')
+                    for position_group in regex.finditer(journal_position_regex, number_group.group('positions'), regex.MULTILINE):
+                        position = position_group.group('position')
+                        ref_data['journals'].append({'year': year,
+                                                     'number': number,
+                                                     'position': position})
+        else:
+            ref_data['year'] = ref.group('year')
+            ref_data['title'] = format_title(ref.group('title'))
+            ref_data['journals'] = []
+            for year_group in regex.finditer(journal_year_groups_regex, ref.group('journal_details'), regex.MULTILINE):
+                year = year_group.group('journal_year')
+                for journal_data in regex.finditer(journal_numbers_regex, year_group.group('numbers'), regex.MULTILINE):
+                    for position_group in regex.finditer(journal_position_regex, journal_data.group('positions'), regex.MULTILINE):
+                        ref_data['journals'].append({'number': journal_data.group('number'),
+                                                     'position': position_group.group('position'),
+                                                     'year': year if year is not None else ref_data['year']})
         references.append(ref_data)
     return references
 
