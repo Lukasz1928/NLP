97d750ec142089d3a557ce86d08e5aeca1c447c3
Lukasz1928 <Lukasz19281@gmail.com>
2019-04-08 11:56:51 +0200

    LLR done

--- lab4/src/file_utils.py
+++ lab4/src/file_utils.py
@@ -22,11 +22,14 @@ def get_data():
     return data
 
 
-def save_results(bigrams, pmis):
+def save_results(bigrams, pmis, llrs):
     with open('results/bigrams.txt', 'w', encoding='utf-8') as f:
         for b in sorted(bigrams.items(), key=lambda b: b[1], reverse=True)[:30]:
             f.write('{}\n'.format(b))
     with open('results/results.txt', 'w', encoding='utf-8') as f:
-        f.write('Top bigrams PMI:\n')
+        f.write('Top bigrams by PMI:\n')
         for p in pmis:
             f.write('\t{}, {}\n'.format(p[0], p[1]))
+        f.write('\nTop bigrams by LLR:\n')
+        for l in llrs:
+            f.write('\t{}, {}\n'.format(l[0], l[1]))
--- lab4/src/main.py
+++ lab4/src/main.py
@@ -16,7 +16,7 @@ def calculate_unigrams_and_bigrams(data):
     unigrams = {}
     for d in data:
         text = prepare_text(d)
-        tokens = nltk.word_tokenize(text, 'polish')
+        tokens = text.split(" ")
         for t in tokens:
             if t in unigrams.keys():
                 unigrams[t] += 1
@@ -44,14 +44,40 @@ def calculate_PMI(unigrams, bigrams):
     return pmis
 
 
+vf = np.vectorize(lambda x, N: (x / N) * np.log((x / N) + (1 if x == 0 else 0)))
+
+
+def H(k):
+    return np.sum(vf(k, np.sum(k)))
+
+
+def LLR(k):
+    return 2 * np.sum(k) * (H(k) - (H(np.sum(k, axis=0)) + H(np.sum(k, axis=1))))
+
+
+def calculate_LLR(unigrams, bigrams):
+    llrs = {}
+    s = sum(bigrams.values())
+    for bigram in bigrams.keys():
+        k = np.zeros([2, 2])
+        k[0][0] = bigrams[bigram]  # both words together
+        k[0][1] = unigrams[bigram[1]] - bigrams[bigram]  # only first word
+        k[1][0] = unigrams[bigram[0]] - bigrams[bigram]  # only second word
+        k[1][1] = s - (k[0][0] + k[0][1] + k[1][0])  # neither word
+        llrs[bigram] = LLR(k)
+    return llrs
+
+
 def main():
     data = get_data()
     unigrams, bigrams = calculate_unigrams_and_bigrams(data.values())
     pmis = calculate_PMI(unigrams, bigrams)
     sorted_pmis = sorted([(k, v) for (k, v) in pmis.items()], key=lambda x: x[1], reverse=True)[:30]
 
-    
-    save_results(bigrams, sorted_pmis)
+    llrs = calculate_LLR(unigrams, bigrams)
+    sorted_llrs = sorted([(k, v) for (k, v) in llrs.items()], key=lambda x: x[1], reverse=True)[:30]
+
+    save_results(bigrams, sorted_pmis, sorted_llrs)
 
 
 if __name__ == "__main__":
