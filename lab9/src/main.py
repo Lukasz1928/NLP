19ba43649672c8a2e3dd8e8ecb661a1681871436
Lukasz1928 <Lukasz19281@gmail.com>
2019-05-14 18:49:27 +0200

    Finished basic NER part

--- lab9/src/main.py
+++ lab9/src/main.py
@@ -1,10 +1,12 @@
 from src.file_utils import get_random_files
-from src.ner import upload_file
+from src.ner import start_processing_file, get_status, get_result, get_processed
 
 
 def main():
-    data = get_random_files(100)
-    upload_file(data[list(data.keys())[0]])
+    data = get_random_files(2)
+    upload_ids = [start_processing_file(text) for name, text in data.items()]
+    named = get_processed(upload_ids)
+    print(named[0])
 
 
 if __name__ == '__main__':
