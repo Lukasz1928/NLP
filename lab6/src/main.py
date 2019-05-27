8491cda3b4b12e30d39b626e04f237fcd9781955
Lukasz1928 <Lukasz19281@gmail.com>
2019-05-27 23:30:49 +0200

    Task 7 (probably) done

--- lab6/src/main.py
+++ lab6/src/main.py
@@ -1,16 +1,24 @@
 from src.meanings import find_meanings_and_synonyms
 from src.hypernymy import find_hypernymy_relation_closure
 from src.hyponyms import find_direct_hyponyms, find_second_order_hyponyms
-from src.relations import find_relations_in_group
+from src.relations import find_relations_in_group, get_word_id, get_word_relations
 
 
 def main():
+    # a = get_word_id('kolizja', 2)[0]
+    # print(get_word_id('bezkolizyjny', 2, None))
+    # print(get_word_relations(a))
+    # exit()
+
     # find_meanings_and_synonyms('szkoda')  # task 3
     # find_hypernymy_relation_closure('wypadek drogowy')  # task 4
     # find_direct_hyponyms('wypadek', 1)  # task 5
     # find_second_order_hyponyms('wypadek', 1)  # task 6
-    find_relations_in_group([('szkoda', 2), ('starata', 1), ('szkoda majątkowa', 1), ('uszczerbek na zdrowiu', 1),
-                             ('krzywda', 1), ('niesprawiedliwość', 1), ('nieszczęście', 2)])
+    # find_relations_in_group([('szkoda', 2), ('strata', 1), ('szkoda majątkowa', 1), ('uszczerbek na zdrowiu', 1),
+    #                          ('krzywda', 1), ('niesprawiedliwość', 1), ('nieszczęście', 2)], "group1") # task 7i
+    find_relations_in_group([('wypadek', 1), ('wypadek komunikacyjny', 1), ('kolizja', 2),
+                             ('zderzenie', 2), ('kolizja drogowa', 1), ('bezkolizyjny', 2),
+                             ('katastrofa budowlana', 1), ('wypadek drogowy', 1)], "group2") # task 7ii
 
 
 if __name__ == "__main__":
--- lab6/src/relations.py
+++ lab6/src/relations.py
@@ -1,3 +1,4 @@
+import matplotlib
 import networkx
 import requests
 import matplotlib.pyplot as plt
@@ -20,56 +21,65 @@ def get_word_relations(word_id):
         for s in r['senses']:
             rels.add((s['id'], word_id))
             labels[(s['id'], word_id)] = relation
+    for r in rels_out:
+        relation = get_relation(r['relation_id'])
+        for s in r['senses']:
+            rels.add((word_id, s['id']))
+            labels[(word_id, s['id'])] = relation
     return rels, labels
 
 
-def get_word_id(word, word_id):
+def get_word_id(word, word_id, part_of_speech='noun_pl'):
     words = requests.get(get_url('lexemes/{}'.format(word))).json()
-    searched_sense = [w['sense_id'] for w in words if w['part_of_speech'] == 'noun_pl' and w['lemma'] == word][0]
+    if part_of_speech is None:
+        searched_sense = [w['sense_id'] for w in words if w['lemma'] == word][0]
+    else:
+        searched_sense = [w['sense_id'] for w in words if w['part_of_speech'] == 'noun_pl' and w['lemma'] == word][0]
     senses = requests.get(get_url('senses/{}'.format(searched_sense))).json()
     if word_id is None:
-        ids = [w['id'] for w in senses['homographs'] if w['part_of_speech'] == 'noun_pl']
+        if part_of_speech is None:
+            ids = [w['id'] for w in senses['homographs']]
+        else:
+            ids = [w['id'] for w in senses['homographs'] if w['part_of_speech'] == 'noun_pl']
     else:
-        ids = [w['id'] for w in senses['homographs'] if w['part_of_speech'] == 'noun_pl' and w['sense_index'] == word_id]
+        if part_of_speech is None:
+            ids = [w['id'] for w in senses['homographs'] if w['sense_index'] == word_id]
+        else:
+            ids = [w['id'] for w in senses['homographs'] if w['part_of_speech'] == 'noun_pl' and w['sense_index'] == word_id]
     return ids
 
 
-def find_relations_in_group(words):
+def find_relations_in_group(words, name):
     word2id = {w: None for w in words}
     id2word = {}
     for w in words:
         try:
-            word_id = get_word_id(w[0], word_id=w[1])[0]
+            word_id = get_word_id(w[0], word_id=w[1], part_of_speech=None)[0]
         except IndexError:
             pass
         else:
             word2id[w] = word_id
             id2word[word_id] = w
-
     graph_vertices = set(words)
-    graph_edges = set()
+    graph_edges = []
     edge_labels = {}
-
-    print(graph_vertices)
 
     for w in id2word.values():
         rels, labels = get_word_relations(word2id[w])
-        print(rels)
         for r in rels:
-            if r[1] in word2id.values():
-                try:
-                    named_r = (id2word[r[0]], id2word[r[1]])
-                except KeyError:
-                    pass
-                else:
-                    graph_edges.add(named_r)
-                    edge_labels[named_r] = labels[r]
+            if r[0] in word2id.values() and r[1] in word2id.values():
+                named_r = (id2word[r[0]], id2word[r[1]])
+                graph_edges.append(named_r)
+                edge_labels[named_r] = labels[r]
 
-    g = networkx.DiGraph()
+    g = networkx.MultiDiGraph()
     g.add_nodes_from(graph_vertices)
     g.add_edges_from(graph_edges)
-    pos = networkx.spring_layout(g)
+    pos = networkx.circular_layout(g)
     plt.subplot(121)
-    networkx.draw(g, pos, edge_labels=edge_labels, with_labels=True, font_size=8)
-    networkx.draw_networkx_edge_labels(g, pos, edge_labels=edge_labels, with_labels=True, font_size=8)
-    plt.show()
+    networkx.draw(g, pos, edge_labels=edge_labels, with_labels=True, font_size=6)
+    networkx.draw_networkx_edge_labels(g, pos, edge_labels=edge_labels, with_labels=True, font_size=6)
+    plt.xlim(1.2 * min([x for x, y in pos.values()]), 1.2 * max([x for x, y in pos.values()]))
+    plt.ylim(1.2 * min([y for x, y in pos.values()]), 1.2 * max([y for x, y in pos.values()]))
+    plt.savefig('results/{}.png'.format(name))
+    plt.clf()
